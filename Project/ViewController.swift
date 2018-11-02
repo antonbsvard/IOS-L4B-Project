f5fc8db0a0e0760612e5c37723fcb77f8d30d674
Oscar <o.stenqvist@gmail.com>
2018-11-02 14:44:24 +0100

    Test

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -17,12 +17,33 @@ class ViewController: UIViewController {
         startQuizBtn.clipsToBounds = true
         startQuizBtn.backgroundColor = UIColor.lightGray
         // Do any additional setup after loading the view, typically from a nib.
+        self.getQuestions()
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.startQuizBtn.layer.cornerRadius = self.startQuizBtn.frame.width * 0.1
     }
 
+    func getQuestions() -> Void {
+        print("GetQuestions()")
+        let jsonURLAsString = "https://opentdb.com/api.php?amount=10&type=multiple"
+        guard let url = URL(string: jsonURLAsString) else {
+            print("No URL")
+            return
+            
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                print("No data")
+                return
+                
+            }
+            let dataAsString = String(data: data, encoding: .utf8)
+            print(dataAsString)
+            
+        }.resume()
+    }
 
 }
 
