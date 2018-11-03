65aeac9a7effabe26342805918df632298270d9c
Oscar <o.stenqvist@gmail.com>
2018-11-03 10:51:38 +0100

    Ny db klass som löser kommunikation med databasen, obs den är async!

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -18,50 +18,15 @@ class ViewController: UIViewController {
         startQuizBtn.clipsToBounds = true
         startQuizBtn.backgroundColor = UIColor.lightGray
         // Do any additional setup after loading the view, typically from a nib.
-        self.getQuestions()
+        let db = opentdb()
+        db.getQuestions()
+//        print(questions)
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.startQuizBtn.layer.cornerRadius = self.startQuizBtn.frame.width * 0.1
     }
 
-    func getQuestions() -> Void {
-        print("GetQuestions()")
-        let jsonURLAsString = "https://opentdb.com/api.php?amount=10&type=multiple"
-        guard let url = URL(string: jsonURLAsString) else {
-            print("No URL")
-            return
-            
-        }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                print("No data")
-                return
-                
-            }
-            do {
-            guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any] else {fatalError("OPPS")}
-                
-//                print(json["results"]!)
-                let results = json["results"]
-                for object in (results as! NSArray) {
-                    for item in object as! NSDictionary{
-                        print("Key: " + (item.key as! String))
-                        print("Value: " + (item.value as! String))
-                        break
-                    }
-                    break
-                }
-            } catch{
-                print("Inte BRa!")
-            }
-            
-//            let dataAsString = String(data: data, encoding: .utf8)
-//            print(dataAsString)
-            
-        }.resume()
-    }
 
 }
 
